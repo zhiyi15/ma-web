a87a818c6c112f1f85d842bcd04674cdadb5cca3
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-02 18:52:39 -0400

    footer complete

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -14,10 +14,8 @@ const navigation = [
   },
 ]
 
-const chickenEmoji = String.fromCharCode(128022)
-
-
-
+const chickenEmoji = '\u{1F414}'
+const redHeartEmoji = '\u{2764}\u{FE0F}'
 
 export default function Footer() {
   return (
@@ -28,21 +26,16 @@ export default function Footer() {
           {navigation.map((item) => (
             <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
               <span className="sr-only">{item.name}</span>
-              <item.icon className="h-6 w-6" aria-hidden="true" />
+              <item.icon className="h-10 w-10" aria-hidden="true" />
             </a>
           ))}
         </div>
-
-        <div className="mt-8 md:order-1 md:mt-0">
-          <span className="text-center text-xs leading-5 text-gray-500"> Made with </span>
-            
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="red" className="w-6 h-6">
-              <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
-            </svg>
-          <span className="text-center text-xs leading-5 text-gray-500"> 
-            by some chickens 
-
-          </span>
+        <div className="mt-8 w-fit flex flex-row mx-auto md:order-1 md:mt-0">
+          <span className="text-lg text-gray-500"> Made with </span>
+          <span className='text-xl'>''{redHeartEmoji}''</span>
+          <span className="text-lg text-gray-500">
+            by some</span>   <span className='text-xl'>''{chickenEmoji}</span>
+          <span className="text-lg text-gray-500">s</span>
         </div>
       </div>
     </footer>
