6bdcdc4d04bddb6fd8958ad79f968c8762c7bb7a
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-02 17:39:48 -0400

    favicon and screenshot added

--- pages/components/Download.tsx
+++ pages/components/Download.tsx
@@ -2,11 +2,21 @@ export default function Download() {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
+
+        <img
+          className="h-72 w-auto rounded-lg"
+          src="./shot2.png"
+          alt="Screenshot"
+          width={2000}
+          height={2000}
+        />
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Ready to dive in?
           <br />
-          Start your free trial today.
+          Download today!
         </h2>
+
+        
         <div className="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
           <a
             href="#"
