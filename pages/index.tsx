d0650b7f293b00f8a24e44f47c1b1d32aebda5b2
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-02 18:35:51 -0400

    minor change for smooth scrolling

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,13 +79,13 @@ export default function Home() {
                 <div className="-my-6 divide-y divide-gray-500/10">
                   <div className="space-y-2 py-6">
                     {navigation.map((item) => (
-                      <a
+                      <Link
                         key={item.name}
-                        href={item.href}
+                        to={item.to} offset={item.offset} smooth={true}
                         className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                       >
                         {item.name}
-                      </a>
+                      </Link>
                     ))}
                   </div>
 
