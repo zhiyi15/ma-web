b63ab0a8d1e4e0dfa80958fcc2147caaabb9dc6b
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-02 18:01:13 -0400

    react icons added and download section complete

--- pages/components/Download.tsx
+++ pages/components/Download.tsx
@@ -1,7 +1,9 @@
+import { BsApple, BsUbuntu, BsWindows } from 'react-icons/bs'
+
 export default function Download() {
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
+      <div className="mx-auto max-w-7xl px-6 py-6 sm:py-10 lg:flex lg:items-center lg:justify-between lg:px-8">
 
         <img
           className="h-72 w-auto rounded-lg"
@@ -10,19 +12,33 @@ export default function Download() {
           width={2000}
           height={2000}
         />
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Ready to dive in?
+        <h2 className="text-3xl font-bold tracking-tight pt-5 text-gray-900 sm:text-4xl">
+          Always Free, 
+          <br />
+          Always Open Source,
           <br />
           Download today!
         </h2>
 
-        
+
         <div className="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
           <a
             href="#"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="rounded-md bg-[#06d6a0] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#06d69ec9] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-         
+         <BsApple size={30}/>
+          </a>
+          <a
+            href="#"
+            className="rounded-md bg-[#06d6a0] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#06d69ec9] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            <BsWindows size={30} />
+          </a>
+          <a
+            href="#"
+            className="rounded-md bg-[#06d6a0] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#06d69ec9] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            <BsUbuntu size={30} />
           </a>
         </div>
       </div>
