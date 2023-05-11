dbb14cf4cf5a77b82b94825af8ee037f0c06a746
zhiyi15 <zhiyihuang1994@gmail.com>
2023-05-11 15:13:32 -0700

    tech stack

--- pages/components/Stacks.tsx
+++ pages/components/Stacks.tsx
@@ -47,7 +47,7 @@ export default function Stack() {
   <div className="bg-white py-24 sm:py-32">
   <div className="mx-auto max-w-7xl px-6 lg:px-8">
   <div className="mx-auto max-w-2xl lg:mx-0">
-  <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Tech Stacks</h2>
+  <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Tech Stack</h2>
   </div>
       <ul
           role="list"
